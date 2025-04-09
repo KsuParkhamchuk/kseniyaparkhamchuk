4d0d44363b76da6ebff5c67dd9f76656936ee6fe
KsuParkhamchuk <k.parkhamchuk@gmail.com>
2025-04-09 17:53:14 -0500

    change vercel config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverRuntimeConfig: {
+    maxDuration: 20,
+  }
 };
 
 export default nextConfig;
