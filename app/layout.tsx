c9488c27bc4021f9bcc684312ee9e8412915686b
KsuParkhamchuk <k.parkhamchuk@gmail.com>
2025-07-19 22:12:08 -0500

    improve main page description

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Image from "next/image";
 import Link from "next/link";
-import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/react"
-
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
   title: "Kseniya Parkhamchuk",
@@ -23,23 +22,54 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <div className="container">
-        <header>
-          <Link href="/" className="header-name">Kseniya Parkhamchuk</Link>
-          <div className="header-contact">
+          <header>
+            <Link href="/" className="header-name">
+              Kseniya Parkhamchuk
+            </Link>
+            <div className="header-contact">
               <div className="social-links">
-                <a href="https://x.com/xena_pk" target="_blank" rel="noopener noreferrer">
-                  <Image src="/icons/x.svg" alt="X Profile" width={24} height={24} />
+                <a
+                  href="https://x.com/xena_pk"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Image
+                    src="/icons/x.svg"
+                    alt="X Profile"
+                    width={24}
+                    height={24}
+                  />
                 </a>
-                <a href="https://github.com/KsuParkhamchuk" target="_blank" rel="noopener noreferrer">
-                  <Image src="/icons/github.svg" alt="GitHub Profile" width={24} height={24} />
+                <a
+                  href="https://github.com/KsuParkhamchuk"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Image
+                    src="/icons/github.svg"
+                    alt="GitHub Profile"
+                    width={24}
+                    height={24}
+                  />
                 </a>
-                <a href="https://www.linkedin.com/in/kparkhamchuk/" target="_blank" rel="noopener noreferrer">
-                  <Image src="/icons/linkedin.svg" alt="LinkedIn Profile" width={24} height={24} />
+                <a
+                  href="https://www.linkedin.com/in/kparkhamchuk/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <Image
+                    src="/icons/linkedin.svg"
+                    alt="LinkedIn Profile"
+                    width={24}
+                    height={24}
+                  />
                 </a>
               </div>
-              <a href="mailto:k.parkhamchuk@gmail.com" className="email-link">k.parkhamchuk@gmail.com</a>
+              <a href="mailto:k.parkhamchuk@gmail.com" className="email-link">
+                k.parkhamchuk@gmail.com
+              </a>
             </div>
-        </header>
+          </header>
           {children}
         </div>
         <SpeedInsights />
--- app/whoami/page.tsx
+++ app/whoami/page.tsx
@@ -5,66 +5,88 @@ import "./styles.css";
 import ProjectCard from "../_components/projectCard/ProjectCard";
 
 export default function WhoAmIPage() {
-    return (
-        <main className="who-am-i-container">
-            <h1 className="page-title">Who am I?</h1>
-                <div className="profile-container">
-                    <Image 
-                        src="/me.png" 
-                        alt="Kseniya Parkhamchuk" 
-                        width={300} 
-                        height={400}
-                        className="profile-image"
-                    />
-                    <div className="profile-text">
-                        <Image 
-                            src="/cat.png" 
-                            alt="Cat" 
-                            width={200} 
-                            height={200}
-                            className="cat-drawing"
-                        />
-                        
-                        <p className="greeting">Hello! I am <strong>Kseniya</strong>. Thanks for visiting my page.</p>
-                        
-                        <p className="bio-text">
-                        I’m a software engineer who started out building web applications—until I discovered that AI is way more exciting.
-                            So now I am exploring each and every detail of machine 
-                                learning. My focus and the biggest interest is <strong>NLP</strong>, 
-                                <strong>transformers and their optimisations</strong>. I am also eager to 
-                            learn more about <strong>human-AI collaboration</strong>  and <strong>mechanistic interpretability</strong> field.
-                        </p>
-                        
-                        <p className="bio-text">
-                            I’m not great at describing myself, so it’s best to check out what I’ve worked on instead:
-                        </p>
-                    </div>
-                </div>    
-                <Link href="/assets/Kseniya_Parkhamchuk_CV.pdf" className="resume-button profile-resume-btn" target="_blank" rel="noopener noreferrer">
-                        Resume
-                </Link>
-                
-            
-            <h2 className="section-title">Latest projects</h2>
-            
-            <div className="projects-grid">
-                <ProjectCard 
-                    title="GPT-2"
-                    description="with custom layers implementation"
-                    githubUrl="https://github.com/KsuParkhamchuk/gpt-2"
-                />
-                 <ProjectCard 
-                    title="vLLM chat comparison"
-                    description="launching several vllm servers with several models on several GPUs"
-                    githubUrl="https://github.com/KsuParkhamchuk/vllm-mm-chat-comparison"
-                />
-                <ProjectCard 
-                    title="Gemma3-sft"
-                    description="in attempts of building custom dataset"
-                    githubUrl="https://github.com/KsuParkhamchuk/gemma3-sft"
-                />
-               
-            </div>
-        </main>
-    );
+  return (
+    <main className="who-am-i-container">
+      <h1 className="page-title">Who am I?</h1>
+      <div className="profile-container">
+        <Image
+          src="/me.png"
+          alt="Kseniya Parkhamchuk"
+          width={300}
+          height={400}
+          className="profile-image"
+        />
+        <div className="profile-text">
+          <Image
+            src="/cat.png"
+            alt="Cat"
+            width={200}
+            height={200}
+            className="cat-drawing"
+          />
+
+          <p className="greeting">
+            Hello! I am <strong>Kseniya</strong>.
+          </p>
+
+          <p className="bio-text">
+            I’m a <strong>senior software engineer</strong> who started out building web
+            applications. I still build them, but now my preferences are in
+            solving interesting technical challenges alongside people who get
+            equally excited about them. Recently I discovered that{" "}
+            <strong>AI is super exciting</strong> in both ways: as a separate
+            direction for development and in integration with excisting
+            solutions. So now I am exploring each and every detail in NLP and
+            LLMs. I am focusing a lot on understanding the details and thinking about
+            how we can apply it for the better.
+          </p>
+          <p>
+            I am making some notes about my findings in my{" "}
+            <Link href="/notebook" className="notebook-btn">
+              Blog
+            </Link>
+            . I am always open to talking and exchanging ideas, feel free to talk to
+            me on{" "}
+            <Link
+              href="https://x.com/xena_pk"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              X
+            </Link>
+          </p>
+          <p></p>
+          <p className="bio-text">My latest work is below:</p>
+        </div>
+      </div>
+      <Link
+        href="/assets/Kseniya_Parkhamchuk_CV.pdf"
+        className="resume-button profile-resume-btn"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Resume
+      </Link>
+
+      <h2 className="section-title">Latest projects</h2>
+
+      <div className="projects-grid">
+        <ProjectCard
+          title="GPT-2"
+          description="with custom layers implementation"
+          githubUrl="https://github.com/KsuParkhamchuk/gpt-2"
+        />
+        <ProjectCard
+          title="vLLM chat comparison"
+          description="launching several vllm servers with several models on several GPUs"
+          githubUrl="https://github.com/KsuParkhamchuk/vllm-mm-chat-comparison"
+        />
+        <ProjectCard
+          title="Gemma3-sft"
+          description="in attempts of building custom dataset"
+          githubUrl="https://github.com/KsuParkhamchuk/gemma3-sft"
+        />
+      </div>
+    </main>
+  );
 }
